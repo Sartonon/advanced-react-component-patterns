098563830a6e8079859063f483cccc3247f59ee3
Santeri Rusila <santtu_1993@hotmail.com>
2017-12-04 21:39:36 +0200

    compound component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Switch from 'material-ui/Toggle';
 import './App.css';
 
+function ToggleOn({ on, children }) {
+  return on ? children : null;
+}
+
+function ToggleOff({ on, children }) {
+  return on ? null : children;
+}
+
+function ToggleButton({ on, toggle, ...props }) {
+  return <Switch toggled={on} onClick={toggle} {...props} />;
+}
+
 class Toggle extends Component {
+  static On = ToggleOn;
+  static Off = ToggleOff;
+  static Button = ToggleButton;
+
   static defaultProps = {
     onToggle: () => {}
   }
@@ -17,14 +33,16 @@ class Toggle extends Component {
   }); 
 
   render() {
-    const { on } = this.state;
-
-    return (
-      <Switch 
-        toggled={on} 
-        onClick={this.toggle} 
-      />
+    const children = React.Children.map(
+      this.props.children,
+      child => 
+        React.cloneElement(child, {
+          on: this.state.on,
+          toggle: this.toggle
+        })
     );
+
+    return <div>{children}</div>;
   }
 }
 
@@ -32,7 +50,11 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Toggle onToggle={on => console.log("toggle", on)}  />
+        <Toggle onToggle={on => console.log("toggle", on)}>
+          <Toggle.On>The button is on</Toggle.On>
+          <Toggle.Button />
+          <Toggle.Off>The button is off</Toggle.Off>
+        </Toggle>
       </MuiThemeProvider>
     )
   }
